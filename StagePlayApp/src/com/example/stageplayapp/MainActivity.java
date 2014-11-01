4caadb081f9dd4e5ff8deb51f7d4733bb4c32f34
Sagar Deo <smd522@nyu.edu>
2014-11-01 19:43:59 -0400

    Created all Layouts and also passed intent from main to Playlist activity.

--- StagePlayApp/src/com/example/stageplayapp/MainActivity.java
+++ StagePlayApp/src/com/example/stageplayapp/MainActivity.java
@@ -1,8 +1,7 @@
 package com.example.stageplayapp;
-
-import android.support.v7.app.ActionBarActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,7 +9,8 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class MainActivity extends ActionBarActivity {
-Button Resume,AllPlays,Settings,About;
+	Button Resume, AllPlays, Settings, About;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -19,59 +19,64 @@ Button Resume,AllPlays,Settings,About;
 		AllPlays = (Button) findViewById(R.id.button2);
 		Settings = (Button) findViewById(R.id.button3);
 		About = (Button) findViewById(R.id.button4);
-		
+
 		Bundle b = new Bundle();
 		b = getIntent().getExtras();
-		
+
 		Resume.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				startResumeActivity();// TODO Auto-generated method stub
-				
+
 			}
 		});
 		AllPlays.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				startAllPlaysActivity();// TODO Auto-generated method stub
-				
+
 			}
 		});
 		Settings.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				startSettingsActivity();// TODO Auto-generated method stub
-				
+
 			}
 		});
 		About.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				startAboutActivity();// TODO Auto-generated method stub
-				
+
 			}
 		});
 	}
-	public void startResumeActivity(){
-		Intent i = new Intent(this,PlayWatchActivity.class);
+
+	public void startResumeActivity() {
+		Intent i = new Intent(this, PlayWatchActivity.class);
 		startActivity(i);
 	}
-	public void startAllPlaysActivity(){
+
+	public void startAllPlaysActivity() {
 		System.out.println("In this activity");
-		Intent i = new Intent(this,PlayListActivity.class);
+		Intent i = new Intent(this, PlayListActivity.class);
 		startActivity(i);
 	}
-	public void startSettingsActivity(){
-		
+
+	public void startSettingsActivity() {
+
 	}
-	public void startAboutActivity(){
-		Intent i = new Intent(this,AboutActivity.class);
+
+	public void startAboutActivity() {
+		Intent i = new Intent(this, AboutActivity.class);
 		startActivity(i);
 	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
--- StagePlayApp/src/com/example/stageplayapp/PlayListActivity.java
+++ StagePlayApp/src/com/example/stageplayapp/PlayListActivity.java
@@ -1,5 +1,6 @@
 package com.example.stageplayapp;
 
+
 import android.app.Activity;
 import android.os.Bundle;
 
