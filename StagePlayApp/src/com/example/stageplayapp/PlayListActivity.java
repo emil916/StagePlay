5bb351f9bdc960e46e1b1c061041954da679d275
Sagar Deo <smd522@nyu.edu>
2014-11-01 17:07:35 -0400

    All the layouts created. Also passed the intent from the main activity to allplaylist activty. Next: I will finish the passing of intents to other activities.

--- StagePlayApp/src/com/example/stageplayapp/PlayListActivity.java
+++ StagePlayApp/src/com/example/stageplayapp/PlayListActivity.java
@@ -1,5 +1,15 @@
 package com.example.stageplayapp;
 
-public class PlayListActivity {
+import android.app.Activity;
+import android.os.Bundle;
 
+public class PlayListActivity extends Activity{
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_playlist);
+	}
 }
+
+
