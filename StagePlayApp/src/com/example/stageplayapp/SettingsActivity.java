58d46beb12542a65110b75b119e176e4bd6d4429
Sagar Deo <smd522@nyu.edu>
2014-11-07 13:33:52 -0500

    Settings activity with Slideshow feature enabled! Transition time up next!

--- StagePlayApp/src/com/example/stageplayapp/SettingsActivity.java
+++ StagePlayApp/src/com/example/stageplayapp/SettingsActivity.java
@@ -2,12 +2,31 @@ package com.example.stageplayapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ToggleButton;
 
 public class SettingsActivity extends Activity {
-		
+		ToggleButton toggle;
 		@Override
 		public void onCreate(Bundle savedInstanceState){
 			super.onCreate(savedInstanceState);
 			setContentView(R.layout.activity_settings);
+			toggle = (ToggleButton) findViewById(R.id.btn_toggle);
+			toggle.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					Boolean on = ((ToggleButton)v).isChecked();
+					if(on){
+						toggle.setTextOff("Disabled");
+						// TODO Auto-generated method stub
+					}
+					else {
+						toggle.setTextOn("Enabled");
+					}
+					
+				}
+			});
 }
 }
