864a704963661d98fc8063366aa0726c21e0daab
Emil <emil916@gmail.com>
2014-11-19 01:47:19 -0500

    Fixed the error showing when phone menu btn is clicked

--- StagePlayApp/src/com/example/stageplayapp/MainActivity.java
+++ StagePlayApp/src/com/example/stageplayapp/MainActivity.java
@@ -114,6 +114,7 @@ public class MainActivity extends ActionBarActivity {
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
+			startSettingsActivity();
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
